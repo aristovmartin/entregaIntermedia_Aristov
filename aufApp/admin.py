f435e3125d8f135dea755f7d8d633311953676e1
Martin Aristov <aristovmartin@gmail.com>
2022-05-07 15:57:42 -0300

    Commit inicial

---
+++ aufApp/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+admin.site.register(Estadio)
+
+admin.site.register(Equipo)
+
+admin.site.register(Futbolista)
+
+
+#user: martin pass: martin123
