9bff0997cf661c6f1855582b719fe93cefa2c37c
Martin Aristov <aristovmartin@gmail.com>
2022-05-07 16:24:36 -0300

    Home page creada

--- entregaIntermedia/aufApp/views.py
+++ entregaIntermedia/aufApp/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
+from .forms import *
 
 # Create your views here.
+
+def home(request):
+    return render(request,'home.html')
