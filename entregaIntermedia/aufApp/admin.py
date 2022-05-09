a6bcf87545218b9f8a61c4b3f0552166c39ff9a0
Martin Aristov <aristovmartin@gmail.com>
2022-05-09 13:45:11 -0300

    'admin.site.register(Entregable)

--- entregaIntermedia/aufApp/admin.py
+++ entregaIntermedia/aufApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Equipos)
+
+admin.site.register(Futbolista)
+
+admin.site.register(Estadio)
+
+# superUsuario: martin password:martin123
